8a01e202214e72f76050ce73502f7ad6aaaf80ce
glenpoin <glenpoin@student.21-school.ru>
2022-11-17 01:45:59 +0300

    create map structure

---
+++ src/map/s21_map.cpp
@@ -0,0 +1,87 @@
+#include "s21_map.h"
+
+namespace s21 {
+// BLOCK: MEMBER FUNCTIONS
+template <class Key, class T>
+map<Key, T>::map() {}
+
+template <class Key, class T>
+map<Key, T>::map(const map& m) {}
+
+template <class Key, class T>
+map<Key, T>::map(map&& m) {}
+
+template <class Key, class T>
+map<Key, T>::~map() {}
+
+template <class Key, class T>
+
+// template <class Key, class T>
+//  int map<Key, T>::operator=(map&& m) {
+//    return 0;
+//  }
+
+// BLOCK: ELEMENT ACCESS
+T& map<Key, T>::at(const Key& key) {
+  return <#initializer #>;
+}
+
+template <class Key, class T>
+T& map<Key, T>::operator[](const Key& key) {
+  return <#initializer #>;
+}
+// BLOCK: ITERATORS
+// template <class Key, class T>
+// iterator map<Key, T>::begin() {
+//  return nullptr;
+//}
+// template <class Key, class T>
+// iterator map<Key, T>::end() {
+//  return nullptr;
+//}
+
+//  BLOCK: CAPACITY
+template <class Key, class T>
+bool map<Key, T>::empty() {
+  return false;
+//}
+//template <class Key, class T>
+//map::size_type map<Key, T>::size() {
+//  return 0;
+//}
+//template <class Key, class T>
+//map::size_type map<Key, T>::max_size() {
+//  return 0;
+//}
+
+// BLOCK:  MODIFIERS
+template <class Key, class T>
+void map<Key, T>::clear() {}
+
+// template <class Key, class T>
+// std::pair<iterator, bool> map<Key, T>::insert(const map::value_type& value) {
+//   return nullptr;
+// }
+
+// template <class Key, class T>
+// std::pair<iterator, bool> map<Key, T>::insert_or_assign(const Key& key,
+//                                                         const T& obj) {
+//   return nullptr;
+// }
+
+template <class Key, class T>
+void map<Key, T>::erase(iterator pos) {}
+
+template <class Key, class T>
+void map<Key, T>::swap(map& other) {}
+
+template <class Key, class T>
+void map<Key, T>::merge(map& other) {}
+
+template <class Key, class T>
+
+// BLOCK: LOOKUP
+bool map<Key, T>::contains(const Key& key) {
+  return false;
+}
+};  // namespace s21
---
+++ src/map/s21_map.h
@@ -0,0 +1,51 @@
+#ifndef CPP2_S21_CONTAINERS_MAP_S21_MAP_H_
+#define CPP2_S21_CONTAINERS_MAP_S21_MAP_H_
+
+#include <cstdio>
+#include <utility>
+
+namespace s21 {
+template <class Key, class T>
+class map {
+ private:
+ public:
+  using key_type = Key;
+  using mapped_type = T;
+  using value_type = std::pair<const key_type, mapped_type>;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  //  using iterator = ;
+  //  using const_iterator = ;
+  using size_type = size_t;
+
+  map();
+  //  map(std::initializer_list<value_type> const &items);
+  map(const map &m);
+  map(map &&m);
+  ~map();
+//  operator=(map &&m);
+
+  T &at(const Key &key);
+  T &operator[](const Key &key);
+
+  iterator begin();
+  iterator end();
+
+  bool empty();
+  size_type size();
+  size_type max_size();
+
+  void clear();
+  std::pair<iterator, bool> insert(const value_type& value);
+  std::pair<iterator, bool> insert(const Key& key, const T& obj);
+  std::pair<iterator, bool> insert_or_assign(const Key& key, const T& obj);;
+  void erase(iterator pos);
+  void swap(map& other);
+  void merge(map& other);
+
+  bool contains(const Key& key);
+};
+
+}  // namespace s21
+
+#endif  //  CPP2_S21_CONTAINERS_MAP_S21_MAP_H_
