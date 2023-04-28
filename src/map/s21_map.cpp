73560ac007f538b2905b45689f8705de376c4784
freiqq <frei3453@gmail.com>
2023-04-28 14:09:41 +0300

    init structure

--- src/map/s21_map.cpp
+++ src/map/s21_map.cpp
@@ -1,87 +1,94 @@
 #include "s21_map.h"
 
 namespace s21 {
-// BLOCK: MEMBER FUNCTIONS
+// BLOCK: CONSTRUCTORS
+
 template <class Key, class T>
 map<Key, T>::map() {}
 
 template <class Key, class T>
-map<Key, T>::map(const map& m) {}
+map<Key, T>::map(const std::initializer_list<value_type> &items) {}
 
 template <class Key, class T>
-map<Key, T>::map(map&& m) {}
+map<Key, T>::map(const map &m) {}
+
+template <class Key, class T>
+map<Key, T>::map(map &&m) noexcept {}
 
 template <class Key, class T>
 map<Key, T>::~map() {}
 
 template <class Key, class T>
-
-// template <class Key, class T>
-//  int map<Key, T>::operator=(map&& m) {
-//    return 0;
-//  }
+typename map<Key, T>::map &map<Key, T>::operator=(map &&m) noexcept {
+  return nullptr;
+}
 
 // BLOCK: ELEMENT ACCESS
-T& map<Key, T>::at(const Key& key) {
-  return <#initializer #>;
+template <class Key, class T>
+T &map<Key, T>::at(const Key &key) {
+  return nullptr;
 }
 
 template <class Key, class T>
-T& map<Key, T>::operator[](const Key& key) {
-  return <#initializer #>;
+T &map<Key, T>::operator[](const Key &key) {
+  return nullptr;
 }
+
 // BLOCK: ITERATORS
-// template <class Key, class T>
-// iterator map<Key, T>::begin() {
-//  return nullptr;
-//}
-// template <class Key, class T>
-// iterator map<Key, T>::end() {
-//  return nullptr;
-//}
+
+//    template<class Key, class T>
+//    iterator map<Key, T>::begin() { return nullptr; }
+
+//    template<class Key, class T>
+//    iterator map<Key, T>::end() { return nullptr; }
 
 //  BLOCK: CAPACITY
+
 template <class Key, class T>
 bool map<Key, T>::empty() {
   return false;
-  //}
-  //template <class Key, class T>
-  //map::size_type map<Key, T>::size() {
-  //  return 0;
-  //}
-  //template <class Key, class T>
-  //map::size_type map<Key, T>::max_size() {
-  //  return 0;
-  //}
+}
 
-  // BLOCK:  MODIFIERS
-  template <class Key, class T>
-  void map<Key, T>::clear() {}
+template <class Key, class T>
+size_t map<Key, T>::size() {
+  return 0;
+}
 
-  // template <class Key, class T>
-  // std::pair<iterator, bool> map<Key, T>::insert(const map::value_type& value) {
-  //   return nullptr;
-  // }
+template <class Key, class T>
+size_t map<Key, T>::max_size() {
+  return 0;
+}
 
-  // template <class Key, class T>
-  // std::pair<iterator, bool> map<Key, T>::insert_or_assign(const Key& key,
-  //                                                         const T& obj) {
-  //   return nullptr;
-  // }
+// BLOCK:  MODIFIERS
 
-  template <class Key, class T>
-  void map<Key, T>::erase(iterator pos) {}
+template <class Key, class T>
+void map<Key, T>::clear() {}
 
-  template <class Key, class T>
-  void map<Key, T>::swap(map& other) {}
+//    template<class Key, class T>
+//    std::pair<iterator, bool> insert(const value_type &value) { return null; }
 
-  template <class Key, class T>
-  void map<Key, T>::merge(map& other) {}
+//    template<class Key, class T>
+//    std::pair<iterator, bool> insert(const value_type &value) { return
+//    nullptr; }
 
-  template <class Key, class T>
+//    template <class Key, class T>
+//    std::pair<iterator, bool> map<Key, T>::insert_or_assign(const Key& key,
+//                                                         const T& obj) {return
+//                                                         nullptr; }
 
-  // BLOCK: LOOKUP
-  bool map<Key, T>::contains(const Key& key) {
-    return false;
-  }
-};  // namespace s21
+//    template<class Key, class T>
+//    void map<Key, T>::erase(iterator pos) {}
+
+//    template<class Key, class T>
+//    void map<Key, T>::swap(map &other) {}
+//
+//    template<class Key, class T>
+//    void map<Key, T>::merge(map &other) {}
+//
+//
+//
+//    // BLOCK: LOOKUP
+
+//    template<class Key, class T>
+//    bool map<Key, T>::contains(const Key &key) { return false; }
+}  // namespace s21
--- src/map/s21_map.h
+++ src/map/s21_map.h
@@ -7,8 +7,6 @@
 namespace s21 {
 template <class Key, class T>
 class map {
- private:
- public:
   using key_type = Key;
   using mapped_type = T;
   using value_type = std::pair<const key_type, mapped_type>;
@@ -18,32 +16,40 @@ class map {
   //  using const_iterator = ;
   using size_type = size_t;
 
+ private:
+  struct node_ {
+    node_ *left_;
+    node_ *right_;
+    node_ *parent_node_;
+    value_type value_;
+  };
+  node_ *root_;
+
+ public:
   map();
-  //  map(std::initializer_list<value_type> const &items);
+  map(std::initializer_list<value_type> const &items);
   map(const map &m);
-  map(map &&m);
+  map(map &&m) noexcept;
   ~map();
-  //  operator=(map &&m);
+  map &operator=(map &&m) noexcept;
 
   T &at(const Key &key);
   T &operator[](const Key &key);
-
-  iterator begin();
-  iterator end();
-
+  //        iterator begin();
+  //        iterator end();
   bool empty();
   size_type size();
   size_type max_size();
 
   void clear();
-  std::pair<iterator, bool> insert(const value_type& value);
-  std::pair<iterator, bool> insert(const Key& key, const T& obj);
-  std::pair<iterator, bool> insert_or_assign(const Key& key, const T& obj);;
-  void erase(iterator pos);
-  void swap(map& other);
-  void merge(map& other);
+  std::pair<iterator, bool> insert(const value_type &value);
+  //  std::pair<iterator, bool> insert(const Key& key, const T& obj);
+  //  std::pair<iterator, bool> insert_or_assign(const Key& key, const T& obj);;
+  //  void erase(iterator pos);
+  //  void swap(map& other);
+  //  void merge(map& other);
 
-  bool contains(const Key& key);
+  bool contains(const Key &key);
 };
 
 }  // namespace s21
