4ebbcccbd4293f8cf2e2f0a94e25de125da5ee1e
freiqq <frei3453@gmail.com>
2023-05-05 01:31:48 +0300

    init trees

---
+++ src/tree/tree.cpp
@@ -0,0 +1,328 @@
+#include "tree.h"
+// TEMP
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <class Key, class T>
+void s21::tree<Key, T>::printTree(tree<Key, T>::node_ *root_,
+                                  std::string indent, bool left) {
+  node_ *iter_node = root_;
+  if (iter_node) {
+    std::cout << indent;
+    std::cout << (left ? "├──" : "└──");
+    if (iter_node->color_) {
+      std::cout << "\033[1;31m(\033[0m" << iter_node->value_.first
+                << "\033[1;31m)\033[0m" << std::endl;
+    } else {
+      std::cout << "\033[1;37m(\033[0m" << iter_node->value_.first
+                << "\033[1;37m)\033[0m" << std::endl;
+    }
+    printTree(iter_node->left_, indent + (left ? "│   " : "    "), true);
+    printTree(iter_node->right_, indent + (left ? "│   " : "    "), false);
+  }
+}
+
+// CONSTRUCTORS
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <class Key, class T>
+s21::tree<Key, T>::tree() {
+  begin_node_ = new node_;
+  end_node_ = new node_;
+}
+
+namespace s21 {
+template <class Key, class T>
+tree<Key, T>::tree(const std::initializer_list<value_type> &items) {
+  begin_node_ = new node_;
+  end_node_ = new node_;
+  if (items.size()) {
+    auto itr = items.begin();
+    while (itr != items.end()) {
+      push(*itr);
+      ++itr;
+    }
+  }
+}
+
+template <class Key, class T>
+tree<Key, T>::~tree() {
+  deleteNode(root_);
+  //  delete begin_node_;
+  //  delete end_node_;
+}
+
+template <class Key, class T>
+void tree<Key, T>::deleteNode(node_ *node) {
+  if (node) {
+    deleteNode(node->left_);
+    deleteNode(node->right_);
+    delete node;
+  }
+}
+
+// MODIFY METHODS
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Key, class T>
+bool s21::tree<Key, T>::push(const value_type value) {
+  bool status = false;
+  if (!root_) {
+    root_ = create_node(value, false);
+    begin_node_ = root_;
+    end_node_->parent_ = root_;
+    root_->right_ = end_node_;
+    size_ += 1;
+  } else {
+    node_ *new_node = create_node(value, true);
+    for (auto itr = begin(); itr != end() && !status; ++itr) {
+      if (!itr.itr_node_->left_ && value.first < itr.itr_node_->value_.first) {
+        itr.itr_node_->left_ = new_node;
+        new_node->parent_ = itr.itr_node_;
+        status = true;
+        size_ += 1;
+      } else if ((itr.itr_node_->right_ == end_node_ ||
+                  !itr.itr_node_->right_) &&
+                 value.first < itr.itr_node_->parent_->value_.first &&
+                 value.first > itr.itr_node_->value_.first) {
+        itr.itr_node_->right_ = new_node;
+        new_node->parent_ = itr.itr_node_;
+        size_ += 1;
+        status = true;
+      }
+    }
+    balanceTree(new_node->parent_);
+    updateSideNodes(new_node);
+  }
+  return status;
+}
+
+template <class Key, class T>
+typename s21::tree<Key, T>::node_ *s21::tree<Key, T>::create_node(
+    const value_type value, bool is_red) {
+  auto node = new node_;
+  node->left_ = nullptr;
+  node->right_ = nullptr;
+  node->parent_ = nullptr;
+  node->color_ = is_red;
+  node->value_ = value;
+  return node;
+}
+
+// BALANCE
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Key, class T>
+void s21::tree<Key, T>::balanceTree(node_ *node) {
+  balanceNode(node);
+  if (node->parent_) {
+    balanceTree(node->parent_);
+  }
+  root_->color_ = false;
+}
+
+template <class Key, class T>
+void s21::tree<Key, T>::balanceNode(tree<Key, T>::node_ *node) {
+  bool is_balanced = false;
+  while (!is_balanced && node != end_node_) {
+    is_balanced = true;
+    if (node && node->right_ && node->right_->color_) {
+      leftTurn(node);
+      node = node->parent_;
+      is_balanced = false;
+    }
+    if (node && node->left_ && node->left_->left_ && node->left_->color_ &&
+        node->left_->left_->color_) {
+      rightTurn(node);
+      node = node->parent_;
+      is_balanced = false;
+    }
+    if (node && node->left_ && node->right_ && !node->color_ &&
+        node->left_->color_ && node->right_->color_) {
+      colorSwap(node);
+      is_balanced = false;
+    }
+  }
+}
+
+template <class Key, class T>
+void s21::tree<Key, T>::rightTurn(tree<Key, T>::node_ *node) {
+  node_ *top_node = node;
+  node_ *bottom_node = node->left_;
+  top_node->color_ = true;
+  bottom_node->color_ = false;
+  if (top_node == root_) {
+    root_ = bottom_node;
+  } else if (top_node->parent_->right_ == top_node) {
+    top_node->parent_->right_ = bottom_node;
+  } else if (top_node->parent_->left_ == top_node) {
+    top_node->parent_->left_ = bottom_node;
+  }
+
+  if (bottom_node->right_) {
+    bottom_node->right_->parent_ = top_node;
+  }
+
+  bottom_node->parent_ = top_node->parent_;
+  top_node->parent_ = bottom_node;
+
+  top_node->left_ = bottom_node->right_;
+  bottom_node->right_ = top_node;
+
+  node = bottom_node;
+  node->right_ = top_node;
+}
+
+template <class Key, class T>
+void s21::tree<Key, T>::leftTurn(tree<Key, T>::node_ *node) {
+  node_ *top_node = node;
+  node_ *bottom_node = node->right_;
+  top_node->color_ = true;
+  bottom_node->color_ = false;
+  if (top_node == root_) {
+    root_ = bottom_node;
+  } else if (top_node->parent_->right_ == top_node) {
+    top_node->parent_->right_ = bottom_node;
+  } else if (top_node->parent_->left_ == top_node) {
+    top_node->parent_->left_ = bottom_node;
+  }
+
+  if (bottom_node->left_) {
+    bottom_node->left_->parent_ = top_node;
+  }
+
+  bottom_node->parent_ = top_node->parent_;
+  top_node->parent_ = bottom_node;
+
+  top_node->right_ = bottom_node->left_;
+  bottom_node->left_ = top_node;
+
+  node = bottom_node;
+  node->left_ = top_node;
+}
+
+template <class Key, class T>
+void s21::tree<Key, T>::colorSwap(tree<Key, T>::node_ *node) {
+  node->color_ = true;
+  node->left_->color_ = false;
+  node->right_->color_ = false;
+}
+
+template <class Key, class T>
+void s21::tree<Key, T>::updateSideNodes(node_ *node) {
+  if (begin_node_->value_.first > node->value_.first) {
+    begin_node_ = node;
+  }
+  if (end_node_->parent_->value_.first < node->value_.first) {
+    end_node_->parent_->right_ = NULL;
+    end_node_->parent_ = node;
+    node->right_ = end_node_;
+  }
+}
+
+template <class Key, class T>
+void s21::tree<Key, T>::erase(s21::tree<Key, T>::iterator pos) {
+  auto itr = begin();
+//  if (pos.itr_node_->right_ && pos.itr_node_->left_) {
+//    while (pos.itr_node_->right_ != end_node_ && pos.itr_node_->right_) }
+  //  auto itr = begin();
+  //  for (auto itr = begin(); itr != end(); ++itr) {
+  //    if (itr == pos) {
+  //
+  //    }
+//    }
+    }
+}
+
+// ITERATORS
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+template <class Key, class T>
+s21::tree<Key, T>::TreeIterator::TreeIterator() {}
+
+template <class Key, class T>
+typename s21::tree<Key, T>::iterator s21::tree<Key, T>::begin() {
+  tree<Key, T>::iterator iterator;
+  auto itr_node = begin_node_;
+  iterator = *itr_node;
+  return iterator;
+}
+
+template <class Key, class T>
+typename s21::tree<Key, T>::iterator s21::tree<Key, T>::end() {
+  tree<Key, T>::iterator iterator;
+  auto itr_node = end_node_;
+  iterator = *itr_node;
+  return iterator;
+}
+
+template <class Key, class T>
+void s21::tree<Key, T>::TreeIterator::operator=(node_ &node_) {
+  itr_node_ = &node_;
+}
+
+template <class Key, class T>
+std::pair<Key, T> &s21::tree<Key, T>::TreeIterator::operator*() {
+  return itr_node_->value_;
+}
+
+template <class Key, class T>
+std::pair<Key, T> *s21::tree<Key, T>::TreeIterator::operator->() {
+  return &(itr_node_->value_);
+}
+
+template <class Key, class T>
+void s21::tree<Key, T>::TreeIterator::operator++() {
+  if (itr_node_ && itr_node_->right_) {
+    itr_node_ = itr_node_->right_;
+    while (itr_node_ && itr_node_->left_) {
+      itr_node_ = itr_node_->left_;
+    }
+  } else {
+    node_ *parent_itr_node = itr_node_->parent_;
+    while (itr_node_->parent_ && itr_node_->parent_->right_ == itr_node_) {
+      itr_node_ = itr_node_->parent_;
+      parent_itr_node = parent_itr_node->parent_;
+    }
+    itr_node_ = parent_itr_node;
+  }
+}
+
+template <class Key, class T>
+void s21::tree<Key, T>::TreeIterator::operator--() {
+  if (itr_node_->left_) {
+    itr_node_ = itr_node_->left_;
+    while (itr_node_->right_) {
+      itr_node_ = itr_node_->right_;
+    }
+  } else {
+    node_ *parent_itr_node = itr_node_->parent_;
+    while (itr_node_->parent_ && itr_node_->parent_->left_ == itr_node_) {
+      itr_node_ = itr_node_->parent_;
+      parent_itr_node = parent_itr_node->parent_;
+    }
+    itr_node_ = parent_itr_node;
+  }
+}
+
+template <class Key, class T>
+bool s21::tree<Key, T>::TreeIterator::operator!=(
+    tree<Key, T>::TreeIterator iterator) {
+  return itr_node_ != iterator.itr_node_;
+}
+
+template <class Key, class T>
+bool s21::tree<Key, T>::TreeIterator::operator==(
+    tree<Key, T>::TreeIterator iterator) {
+  return itr_node_ != iterator.itr_node_;
+}
+
+}  // namespace s21
+
+int main() {
+  s21::tree<int, int> tree;
+  s21::tree<int, int> test;
+  test.push({24, 25});
+  test.push({5, 5});
+  test.push({1, 1});
+  test.push({15, 15});
+  test.push({3, 3});
+  test.push({8, 8});
+  test.printTree(test.get_root(), "", false);
+}
---
+++ src/tree/tree.h
@@ -0,0 +1,98 @@
+
+
+#include <initializer_list>
+#include <sstream>
+#include <utility>
+
+#include "iostream"
+#include "string"
+
+#ifndef CPP2_S21_CONTAINERS_TREE_S21_TREE_H_
+#define CPP2_S21_CONTAINERS_TREE_S21_TREE_H_
+// #include "tree.cpp"
+
+namespace s21 {
+template <class Key, class T>
+class tree {
+  using key_type = Key;
+  using mapped_type = T;
+  using value_type = std::pair<const key_type, mapped_type>;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using size_type = size_t;
+
+  // temp pub
+ private:
+  struct node_ {
+    node_ *left_;
+    node_ *right_;
+    node_ *parent_;
+    std::pair<Key, mapped_type> value_;
+    bool color_;
+  };
+  node_ *root_ = nullptr;
+  node_ *begin_node_ = nullptr;
+  node_ *end_node_ = nullptr;
+  size_type size_ = 0;
+
+ public:
+  class TreeIterator;
+  using iterator = TreeIterator;
+
+  tree();
+  tree(std::initializer_list<value_type> const &items);
+  ~tree();
+
+  T &at(const Key &key);
+  T &operator[](const Key &key);
+
+  iterator begin();
+  iterator end();
+
+  bool empty();
+  size_type size();
+
+  std::pair<iterator, bool> insert(const value_type &value);
+
+  bool contains(const Key &key);
+
+  // rb tree
+  node_ *get_root() { return root_; };
+  void updateSideNodes(node_ *node);
+
+  void initTree();
+  node_ *create_node(value_type value, bool is_red);
+  bool push(value_type value);
+  bool checkBalance();
+  void balanceTree(tree<Key, T>::node_ *node);
+  void balanceNode(tree<Key, T>::node_ *node);
+  void colorSwap(tree<Key, T>::node_ *node);
+  void rightTurn(tree<Key, T>::node_ *node);
+  void leftTurn(tree<Key, T>::node_ *node);
+  void printTree(tree<Key, T>::node_ *root_, std::string indent, bool left);
+  void deleteNode(node_ *node);
+  void erase(s21::tree<Key, T>::iterator pos);
+};
+
+template <class Key, class T>
+class tree<Key, T>::TreeIterator {
+ private:
+  node_ *itr_node_ = NULL;
+
+ public:
+  TreeIterator();
+
+  void operator=(node_ &node_);
+  std::pair<Key, T> &operator*();
+  std::pair<Key, T> *operator->();
+  void operator++();
+  void operator--();
+  bool operator==(tree<Key, T>::TreeIterator iterator);
+  bool operator!=(tree<Key, T>::TreeIterator iterator);
+
+  friend class tree;
+};
+
+}  // namespace s21
+
+#endif  // CPP2_S21_CONTAINERS_TREE_S21_TREE_H_
